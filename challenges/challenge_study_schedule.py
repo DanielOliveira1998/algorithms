7fd3a10b14091253e26c0672b5479cb8426230a6
Daniel Oliveira <danoliveira865@gmail.com>
2023-08-31 14:46:48 -0300

    req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
 def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
-    raise NotImplementedError
+    active_students = 0
+    if target_time is None:
+        return None
+    for student in permanence_period:
+        if(not isinstance(student[0], int) or not isinstance(student[1], int)):
+            return None
+        if(student[0] <= target_time) and (student[1] >= target_time):
+            active_students += 1
+    return active_students
+    
