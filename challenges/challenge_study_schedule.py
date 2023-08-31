532dc9d07a2417ae82d6a69aadf8c614e114fce4
Daniel Oliveira <danoliveira865@gmail.com>
2023-08-31 14:54:45 -0300

    req 1 lint

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -4,9 +4,9 @@ def study_schedule(permanence_period, target_time):
     if target_time is None:
         return None
     for student in permanence_period:
-        if(not isinstance(student[0], int) or not isinstance(student[1], int)):
+        if (not isinstance(student[0], int) or not isinstance(student[1], int)):
             return None
-        if(student[0] <= target_time) and (student[1] >= target_time):
+        if (student[0] <= target_time) and (student[1] >= target_time):
             active_students += 1
     return active_students
     
