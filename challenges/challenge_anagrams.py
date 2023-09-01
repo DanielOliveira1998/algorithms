cb06962714c940b6947a3e2f179920a3328e9fdf
Daniel Oliveira <danoliveira865@gmail.com>
2023-08-31 23:43:13 -0300

    req 5

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,10 @@
 def is_anagram(first_string, second_string):
     """Faça o código aqui."""
-    raise NotImplementedError
+    sorted_first = sorted(first_string.split(), key=str.lower)
+    sorted_second = sorted(second_string.split(), key=str.lower)
+    if len(first_string) == 0 or len(second_string) == 0:
+        return False
+    if (len(first_string) != len(second_string)):
+        return (sorted_first, sorted_second, False)
+    if (sorted_first == sorted_second):
+        return (sorted_first, sorted_second, True)
--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,13 @@
 def find_duplicate(nums):
     """Faça o código aqui."""
-    raise NotImplementedError
+    frequency = {}
+    if len(nums) == 1 or len(nums) == 0:
+        return False
+    for num in nums:
+        if isinstance(num, str) or num < 0:
+            return False
+        if num in frequency:
+            return num
+        else: frequency[num] = 1
+    return False
+
